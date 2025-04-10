4b4cf42c22c6449e0bc7370a7f497733a0e6df5f
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-11 03:15:39 +0530

    fixing no-movie poster

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieCard = ({movie:{title,vote_average,poster_path,original_language,release_date}}) => {
   return (
     <div className='movie-card'>
-        <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : './no-movies.png'} alt={title}/>
+        <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : 'no-movie.png'} alt={title}/>
         <div className='mt-4'>
             <h3>{title}</h3>
             <div className='content'>

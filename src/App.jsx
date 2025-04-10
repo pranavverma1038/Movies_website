473cf0c047c74804453448cb518f863e717e8c71
Pranav Verma <101242598+pranavverma1038@users.noreply.github.com>
2025-04-10 23:07:22 +0530

    adding search and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Search from './components/search.jsx'
 
 
 const App= ()=>{
+  const [searchTerm,setSearchTerm] = useState('');
   return(
     <main>
       <div className="pattern"/>
       <div className='wrapper'>
         <header>
-          <img src='./herp.png' alt="hero Banner"></img>
+          <img src='./hero.png' alt="Hero Banner"></img>
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the hassel</h1>
         </header>
-
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        
       </div>
     </main>
   )
